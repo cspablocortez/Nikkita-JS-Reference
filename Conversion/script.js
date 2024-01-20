cbc1f598718836f605b72a329613073afff51284
Pablo Cortez <cspablocortez@gmail.com>
2024-01-20 14:03:50 -0800

    Update script.js

--- Conversion/script.js
+++ Conversion/script.js
@@ -1,5 +1,19 @@
 function convert(e) {
-    const elementID = e.id;                                                      // this gives us only the ID
-    const degrees = document.getElementById(elementID).value;                    // this is giving us the number currently typed in
-    console.log(`${degrees} input from ${elementID}.`);                          // string interpolation 
+    const elementID = e.id;                                                      
+    const degrees = document.getElementById(elementID).value;                                          
+    const isFahrenheit = elementID === 'fahrenheit';
+
+    const result = isFahrenheit ? getCelsius(degrees) : getFahr(degrees);
+    const targetElement = document.getElementById(isFahrenheit ? 'celsius' : 'fahrenheit'); // grab the opposite box depending on function caller ID
+    targetElement.value = result;
+}
+
+function getCelsius(degrees) {
+    const f = parseFloat(degrees);
+    return Math.floor((5/9) * (f - 32));
+}
+
+function getFahr(degrees) {
+    const c = parseFloat(degrees);
+    return Math.floor(((9/5) * c) + 32);
 }
